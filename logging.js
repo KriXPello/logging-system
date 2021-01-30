dd1f6d333a187492f3824ac05c5964516411ec25
KriXPello <43673109+KriXPello@users.noreply.github.com>
2021-01-30 16:59:26 +0300

    Create logging.js

---
+++ logging.js
@@ -0,0 +1,35 @@
+import { createWriteStream, WriteStream } from 'fs'
+
+const getTime = (): string => {
+  const date = new Date()
+  const dmy = date.toLocaleDateString()
+  const hms = date.toLocaleTimeString()
+
+  return dmy + ' ' + hms
+}
+
+let stream: null | WriteStream = null
+
+export const startLogging = (): void => {
+  const validTime = getTime()
+    .replaceAll('.', '-')
+    .replaceAll(':', ';')
+    .replace(' ', '_')
+    
+  const logName = `./${validTime} log.txt`
+  
+  stream = createWriteStream(logName)
+
+  stream.on('open', () => console.log('Logging started'))
+  stream.on('error', (e) => console.log('Logging error:', e.message))
+}
+
+export const log = (...info: string[]): void => {
+  if (stream)
+    stream.write(`LOG  ${getTime()}  ${info.join(' ')}\n`)
+}
+
+export const error = (...info: string[]): void => {
+  if (stream)
+    stream.write(`ERROR  ${getTime()}  ${info.join(' ')}\n`)
+}
